fe6f96f1769c28a71c7b18ed376da9e3a2b8c221
Angel Castineira Duran <acastineiraduran@danielcastelao.org>
2022-10-14 10:04:38 +0200

    version 1.0 porfin acabada

--- untitled/src/holaMundoJava14Oct/Principal.java
+++ untitled/src/holaMundoJava14Oct/Principal.java
@@ -3,7 +3,7 @@ package holaMundoJava14Oct;
 /**
  * @version 1.0
  */
-public class HolaMundo {
+public class Principal {
     public static void main(String[] args) {
         System.out.println("Ejercicio del 14 de octubre");
     }
